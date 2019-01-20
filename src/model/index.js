740e88078279168f3b4bffba7ebbfe35bf04a8b1
Ethan <qizhenshuai@126.com>
2019-01-20 19:41:42 +0800

    update package.json

--- src/model/index.js
+++ src/model/index.js
@@ -1,11 +1,11 @@
 import common from './common'
 import home from '../pages/index/model'
 import center from '../pages/center/model'
-import detail from '../pages/detail/model'
+// import detail from '../pages/detail/model'
 
 export default [
   common,
   home,
   center,
-  detail
+  // detail
 ]
